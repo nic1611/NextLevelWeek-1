ddf1455851e1ed9743865916598a8bd763bcf9ea
nicolassoffi <nicolassoffi@bne-empregos.com.br>
2020-06-05 09:49:16 -0300

    Recursos adicionais no backend

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -16,7 +16,7 @@ class PointsController {
 
         const trx = await knex.transaction();
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=50',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -30,12 +30,15 @@ class PointsController {
 
         const point_id = insertedIds[0];
 
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id: point_id,
-            };
-        });
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id: point_id,
+                };
+            });
 
         await trx('point_items').insert(pointItems);
 
@@ -56,12 +59,17 @@ class PointsController {
             return response.status(400).json({ message: 'Point not found.' });
         }
 
+        const serializedPoint ={
+            ...point,
+            image_url: `http://192.168.1.12:3333/uploads/${point.image}`
+        }
+
         const items = await knex('items')
             .join('point_items', 'items.id', "=", "point_items.item_id")
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return response.json({ point, items });
+        return response.json({ point: serializedPoint, items });
     }
 
     async index(request: Request, response: Response) {
@@ -78,6 +86,13 @@ class PointsController {
             .where('uf', String(uf))
             .distinct()
             .select('points.*');
+
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.1.12:3333/uploads/${point.image}`,
+            };
+        });
 
         return response.json(points);
     }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import express, { request, response } from 'express';
-import knex from './database/connection';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
@@ -12,7 +16,25 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longiture: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create);
+
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
